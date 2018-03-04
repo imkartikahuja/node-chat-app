93be7cb920ce20bd64883d670b0b000c2520e38f
imkartikahuja <kartik.ahuja24@gmail.com>
2018-03-05 01:27:40 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();    //iniate a request from client to server to open a web socket and keep that connection open
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Kartik',
+    text: 'Hey. This is Kartik.'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});

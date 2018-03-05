8587995a902f844e606f8150783f23e725f00fa7
imkartikahuja <kartik.ahuja24@gmail.com>
2018-03-05 16:23:23 +0530

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,27 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Kartik',
+//     text: 'Hey. This is Kartik.'
+//   }, function (data) {        //acknowledgement to client
+//     console.log('Got it', data);
+//   });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {              //acknowledgement to client
+
+  });
 });
